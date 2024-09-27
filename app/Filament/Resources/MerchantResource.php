582c3dd08d3ce97f17ed2446a23bc83b78a91da0
ar-kun <ar-kun@users.noreply.github.com>
2024-09-27 04:35:18 +0000

    Fix styling (php-cs-fixer & blade formatter github actions)

--- app/Filament/Resources/MerchantResource.php
+++ app/Filament/Resources/MerchantResource.php
@@ -3,15 +3,12 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\MerchantResource\Pages;
-use App\Filament\Resources\MerchantResource\RelationManagers;
 use App\Models\Merchant;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class MerchantResource extends Resource
 {
--- app/Filament/Resources/MerchantResource/Pages/CreateMerchant.php
+++ app/Filament/Resources/MerchantResource/Pages/CreateMerchant.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Resources\MerchantResource\Pages;
 
 use App\Filament\Resources\MerchantResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateMerchant extends CreateRecord
